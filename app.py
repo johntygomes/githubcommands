ceb6f85d5ca080187d13771028ae79aefd224f7c
roadcastIntern <intern@roadcast.in>
2021-06-15 00:24:37 +0530

    subtract back to added line 3

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-
-#Player One Works On Add In Branch Master
-result = num_1 + num_2
-print(result)
-
-
-#Player Two Works On Subtract On Substract Branch
-#git checkout -b subtract      ...create a new branch named subtract
-result = num_1 - num_2
-result = num_1 * num_2
-=======
 #added line 1
 #added line 2
 #added line 3
->>>>>>> ec1fcca... added line 3
