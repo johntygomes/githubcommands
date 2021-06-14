7d02a84785d872bb6fa7efb2ad62bf292c2bfd89
roadcastIntern <intern@roadcast.in>
2021-06-14 23:49:06 +0530

    cleared full file

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-##SIMPLE CALCULATOR GITHUB
-num_1 = int(input("Enter A Number"))
-num_2 = int(input("Enter A Number"))
 
-#Player One Works On Add In Branch Master
-result = num_1 + num_2
-print(result)
-
-
-#Player Two Works On Subtract On Substract Branch
-#git checkout -b subtract      ...create a new branch named subtract
-result = num_1 - num_2
-#Spoilt the code
