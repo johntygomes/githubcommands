d8dfc8faaebd2f9cdb74e96bf82f4a670227fe32
roadcastIntern <intern@roadcast.in>
2021-06-14 22:43:10 +0530

    Add Functionality Added

---
+++ app.py
@@ -0,0 +1,10 @@
+##SIMPLE CALCULATOR GITHUB
+num_1 = int(input("Enter A Number"))
+num_2 = int(input("Enter A Number"))
+
+#Player One Works On Add In Branch Master
+result = num_1 + num_2
+print(result)
+
+
+#Player Two Works On Subtract On Substract Branch
