a823f964c78fc38095eb6d405a53b7cf8f36a65b
roadcastIntern <intern@roadcast.in>
2021-06-17 10:46:09 +0530

    Added add functionality

--- app.py
+++ app.py
@@ -6,6 +6,9 @@
 num_1 = int(input("ENTER A NUMBER"))
 num_2 = int(input("ENTER A NUMBER"))
 #############################
+######################################----ADDING ADD
+add_result = num_1 + num_2
+print(add_result)
 ####################################-----ADDING SUBTRACT
 subtract_result = num_2 - num_1
 print(subtract_result)
