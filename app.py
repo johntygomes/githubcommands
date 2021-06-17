71d518b2cbfb7d26e1210cc0b4c342c6be7c97fa
roadcastIntern <intern@roadcast.in>
2021-06-17 10:39:04 +0530

    Setup Calculator Done

--- app.py
+++ app.py
@@ -2,3 +2,7 @@
 #added line 2
 #added line 3
 #added lin 4
+#############SIMPLE CALCULATOR APP------TAKING INPUT
+num_1 = int(input("ENTER A NUMBER"))
+num_2 = int(input("ENTER A NUMBER"))
+#############################
