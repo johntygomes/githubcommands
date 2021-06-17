ab17a77c31cd187f0c45b5cebb2fbaa0dec776a5
roadcastIntern <intern@roadcast.in>
2021-06-17 10:42:29 +0530

    Subtract Functionality

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 #added line 1
 #added line 2
 #added line 3
-#added lin 4
+#added line 4
 #############SIMPLE CALCULATOR APP------TAKING INPUT
 num_1 = int(input("ENTER A NUMBER"))
 num_2 = int(input("ENTER A NUMBER"))
 #############################
+####################################-----ADDING SUBTRACT
+subtract_result = num_2 - num_1
+print(subtract_result)
+#################################################
