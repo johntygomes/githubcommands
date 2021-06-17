9e0435c08c0da636b880a78600422c86aca9ea69
roadcastIntern <intern@roadcast.in>
2021-06-17 10:51:40 +0530

    4 to 5

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #added line 1
 #added line 2
 #added line 3
-#added line 4
+#added line 5
 #############SIMPLE CALCULATOR APP------TAKING INPUT
 num_1 = int(input("ENTER A NUMBER"))
 num_2 = int(input("ENTER A NUMBER"))
